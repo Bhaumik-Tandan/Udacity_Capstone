9cc78ba196f3bda126a7ddcc9cd7e168c20ea140
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2023-03-24 12:08:24 +0530

    [ADD] dynamic query string

--- PredictionFunction/app.py
+++ PredictionFunction/app.py
@@ -21,8 +21,8 @@ random_forest=loadModel('random_forest.sav')
 def lambda_handler(event, context):
     parameters=event['queryStringParameters'];
     print("Parameters",parameters)
-
-    field=[
+# https://wpgay7f1u9.execute-api.us-east-1.amazonaws.com/Prod/predict?longitude=7&latitude=28.1234&average_cost_for_two=100&has_table_booking=1&has_online_delivery=1&is_delivering_now=1&switch_to_order_menu=1&price_range=1&votes=1000
+    fields=[
         parameters['longitude'],
         parameters['latitude'],
         parameters['average_cost_for_two'],
@@ -33,7 +33,7 @@ def lambda_handler(event, context):
         parameters['price_range'],
         parameters['votes']
     ];
-    print("Field",field)
+    print("Field",fields)
 
     for _ in range(148):
         fields.append(randint(0,1))
